2a3684bba9f548ce22b60c1233e0d45fa1211ad3
LuiCoro <luiacorona029@icloud.com>
2021-10-30 20:25:10 -0700

    'Installed Dependencies' - Express Installed 'Able to GET post & post/:id'

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,43 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router()
+
+const Posts = require('./posts-model')
+
+
+// Need to get all 10 Posts back for this to work
+// GET /api/posts
+router.get('/', (req, res) => {
+  Posts.find()
+      .then(posts => {
+        res.status(200).json(posts)
+      })
+      .catch(error => {
+        res.status(500).json({message: 'The posts information could not be retrieved'})
+      })
+})
+
+
+// GET /api/posts/:id
+// if not found -> Code : 404 {message: 'The post with the specified ID does not exist'}
+// ERROR retrieving from Data Base -> Code 500 {message: 'The post information could not be retrieved'}
+router.get('/:id', (req, res) => {
+
+  const {id} = req.params
+
+  Posts.findById(id)
+      .then(post => {
+
+        if (post) {
+          res.status(200).json(post)
+        } else {
+          res.status(404).json({message: 'The post with the specified ID does not exist'})
+        }
+
+      }).catch(error => {
+    res.status(500).json({message: 'The post information could not be retrieved'})
+  })
+})
+
+module.exports = router
