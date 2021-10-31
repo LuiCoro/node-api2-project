d4eb8fca8ed38b8c9a808317c99683657ff8527d
LuiCoro <luiacorona029@icloud.com>
2021-10-30 21:34:18 -0700

    'PUT Works!' 'TODO:' - DELETE /api/posts/:id - GET /api/posts/:id/comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,39 +5,71 @@ const router = express.Router()
 
 const Posts = require('./posts-model')
 
-
-// Need to get all 10 Posts back for this to work
 // GET /api/posts
-router.get('/', (req, res) => {
-  Posts.find()
-      .then(posts => {
-        res.status(200).json(posts)
-      })
-      .catch(error => {
-        res.status(500).json({message: 'The posts information could not be retrieved'})
-      })
+router.get('/', async (req, res) => {
+  const posts = await Posts.find()
+  if(!posts) {
+    res.status(500).json({ message: "The posts information could not be retrieved" })
+  } else {
+    res.status(200).json(posts)
+  }
 })
-
 
 // GET /api/posts/:id
-// if not found -> Code : 404 {message: 'The post with the specified ID does not exist'}
-// ERROR retrieving from Data Base -> Code 500 {message: 'The post information could not be retrieved'}
-router.get('/:id', (req, res) => {
-
-  const {id} = req.params
-
-  Posts.findById(id)
-      .then(post => {
-
-        if (post) {
-          res.status(200).json(post)
-        } else {
-          res.status(404).json({message: 'The post with the specified ID does not exist'})
-        }
-
-      }).catch(error => {
+router.get('/:id', async (req, res) => {
+  try{
+    const {id} = req.params
+    const post = await Posts.findById(id)
+    if(!post) {
+      res.status(404).json({message: 'The post with the specified ID does not exist'})
+    } else {
+      res.status(200).json(post)
+    }
+  } catch (error) {
     res.status(500).json({message: 'The post information could not be retrieved'})
-  })
+  }
 })
+
+// POST /api/posts
+router.post('/', async(req, res) => {
+  try{
+    const {title, contents} = req.body
+    if(!title || ! contents) {
+      res.status(400).json({message: 'Please provide title and contents for the post'})
+    } else {
+      const newPost = await Posts.insert({title, contents})
+
+      res.status(200).json(newPost)
+    }
+  } catch (error) {
+    res.status(500).json({message: 'There was an error while saving the post to the database'})
+  }
+})
+
+// PUT /api/posts/:id
+
+router.put('/:id', async (req, res) => {
+  try{
+    const {id} = req.params
+    const {title, contents} = req.body
+
+    if(!title || !contents) {
+      res.status(400).json({message: 'Please provide title and contents for the post'})
+    } else {
+      const updatePost = await Posts.update(id, {title, contents})
+
+      if(!updatePost) {
+        res.status(404).json({message: 'The post with the specified ID does not exist'})
+      } else {
+        res.status(200).json(updatePost)
+      }
+    }
+
+  } catch (error) {
+    res.status(500).json({message: 'The post information could not be modified'})
+  }
+})
+
+
 
 module.exports = router
